f37299a70820d76759dcf0ce1cb722ebc4d5ac74
ClimbSky <52294301+engelmallen@users.noreply.github.com>
2021-04-21 12:19:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-let inputBtn = document.getElementsByClassName('inputBtn');
-let inputText = document.getElementById('inputText');
-let sumBtn = document.getElementById('sumBtn');
-
-let val1 = null;
-let val2 = null;
-let total = null;
-
-let typing = ()=>{
-		for (var i = 0; i < inputBtn.length; i++) {
-			inputBtn[i].addEventListener('click', function(a){
-			inputText.value += a.target.value
-
-			})
-	}
-
-}
-typing()
-
-
-
 let getSystems = async () => {
 	const response = await fetch("https://api.rawg.io/api/platforms?key=24c80e1f2a1345e09c8964b0e655605a")
 	

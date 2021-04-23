099d6fc7404815cb7dad9e5f519c585977f190ee
ClimbSky <52294301+engelmallen@users.noreply.github.com>
2021-04-23 07:39:19 -0400

    Search bar working

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+let searchBar = document.getElementById('searchBar')
+let searchBtn = document.getElementById('searchBtn')
+let info = document.getElementById('info')
+
+let systems = null;
+let games = null;
+
 let getSystems = async () => {
 	const response = await fetch("https://api.rawg.io/api/platforms?key=24c80e1f2a1345e09c8964b0e655605a")
 	
@@ -12,10 +19,34 @@ let getGames = async () => {
 	 console.log(games)
 }
 
-
+let makeBlocks = (block) => {
+	let createIt =
+		`<div id="info" class="border w-100 rounded ">
+		<div class="card" style="width: 18rem;">
+			<img src="${block.background_image}" class="card-img-top" alt="...">
+			<div class="card-body">
+			<h5 class="card-title">${block.name}</h5>
+			<p class="card-text">${block.rating}</p>
+			<a href="#" class="btn btn-primary">Go somewhere</a>
+			</div>
+		</div>
+	</div>`;
+	return createIt
+}
 
 getGames()
 getSystems()
+
+searchBtn.addEventListener('click', function(){
+	let makeGames = games.results.map((i)=>{
+		info.innerHTML += makeBlocks(i)
+		
+	})
+	console.log(makeGames);
+})
+
+
+
 
 /*
 GET https://api.rawg.io/api/platforms?key=24c80e1f2a1345e09c8964b0e655605a
